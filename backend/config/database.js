1ec5b8d431f1792f63d045b801b30894113733b5
div-zihad007 <div.sajid007@gmail.com>
2024-08-22 20:18:24 +0600

    Order Page Design Complete

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,5 +4,8 @@ export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://dev_sajid:sajid789@cluster0.zvkgaq4.mongodb.net/food_delivery')
     .then(() => {
         console.log("DB Connected!");
+    })
+    .catch((err) => {
+        console.log("Connection Error!",err);
     });
 } 
