30439f0a5dacffe29615f2ad9d4c1765d5a6f8b0
div-zihad007 <div.sajid007@gmail.com>
2024-08-22 21:27:30 +0600

    Project Complete

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
     <div className="header">
         <div className="header-contents">
-            <h2>Order Your Favourate food header</h2>
+            <h2>Order Your Favourate food here.</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus a debitis aliquid eum earum repudiandae, doloremque sunt suscipit architecto esse. Obcaecati quod rerum eius voluptates debitis enim eaque autem!</p>
             <button>View Menu</button>
         </div>
